120ecfbcbaf4e26dcbf2e01895e82fffe38668c4
htYap <yhtthomas@gmail.com>
2020-06-12 18:59:54 +0800

    improve test case with dataprovider

--- tests/StringHelperTest.php
+++ tests/StringHelperTest.php
@@ -5,11 +5,20 @@ use IpriceHT\StringHelper;
 
 final class StringHelperTest extends TestCase
 {
-    public function testConvertStringUpperCase(): void
-    {
-        $exampleStr = 'my ht';
-        $expectedStr = 'MY HT';
+    public function allUpperCaseDataProvider() {
+        return [
+            ['my ht', 'MY HT'],
+            ['heLlo World', 'HELLO WORLD'],
+            ['hello World', 'HELLO WORLD'],
+            ['heLlO babY', 'HELLO BABY']
+        ];
+    }
 
+    /**
+     * @dataProvider allUpperCaseDataProvider
+     */
+    public function testConvertStringUpperCase($exampleStr, $expectedStr ): void
+    {
         $stringHelper = new StringHelper();
 
         $result = $stringHelper->convertUpperCase($exampleStr);
