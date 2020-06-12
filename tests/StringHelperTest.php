e37d2dcaa56c819c3e04b3e40517e79b4b8acb69
htYap <yhtthomas@gmail.com>
2020-06-12 20:49:59 +0800

    tidy up code

--- tests/StringHelperTest.php
+++ tests/StringHelperTest.php
@@ -7,27 +7,27 @@ final class StringHelperTest extends TestCase
 {
     public function allUpperCaseDataProvider() {
         return [
-            ['my ht', 'MY HT'],
-            ['heLlo World', 'HELLO WORLD'],
-            ['hello World', 'HELLO WORLD'],
-            ['heLlO babY', 'HELLO BABY']
+            'my ht' => ['my ht', 'MY HT'],
+            'heLlo World' => ['heLlo World', 'HELLO WORLD'],
+            'hello World' => ['hello World', 'HELLO WORLD'],
+            'heLlO babY' => ['heLlO babY', 'HELLO BABY']
         ];
     }
 
     public function allOddIndexToUpperCaseDataProvider() {
         return [
-            ['hello silly', 'hElLo sIlLy'],
-            ['heLlo mummy', 'hElLo mUmMy'],
-            ['heLlo dAddy', 'hElLo dAdDy'],
-            ["hello World", "hElLo wOrLd"],
-            ['heLlO babY', 'hElLo bAbY']
+            'hello silly' => ['hello silly', 'hElLo sIlLy'],
+            'heLlo mummy' => ['heLlo mummy', 'hElLo mUmMy'],
+            'heLlo dAddy' => ['heLlo dAddy', 'hElLo dAdDy'],
+            'hello World' => ["hello World", "hElLo wOrLd"],
+            'heLlO babY' => ['heLlO babY', 'hElLo bAbY']
         ];
     }
 
     /**
      * @dataProvider allUpperCaseDataProvider
      */
-    public function testConvertStringUpperCase($exampleStr, $expectedStr ): void
+    public function testConvertStringAllUpperCase($exampleStr, $expectedStr ): void
     {
         $stringHelper = new StringHelper();
 
